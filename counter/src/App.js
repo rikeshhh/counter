c907d8018ff86eecef36b2b4509527a5e8ee0fa8
rikeshhh <70895896+rikeshhh@users.noreply.github.com>
2022-11-10 13:17:46 +0545

    increment and decrement

--- counter/src/App.js
+++ counter/src/App.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 import './App.css'
 import ByTen from './component/add';
 function App() {
-  const [value,setValue] = useState(10);
+  const [value,setValue] = useState(0);
   // const [valueByten,setValueByten]
   // const increment = ()=>{
   //   setValue(value+5);
@@ -15,10 +15,14 @@ function App() {
     {/* <button onClick={increment}>+</button>
       <span>{value}</span>
 <button onClick={decrement}>-</button>  */}
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={1}/>
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={5}/>
 <ByTen valueByten={value} setValueByten={setValue} increaseBy={10}/>
-<ByTen valueByten={value} setValueByten={setValue} increaseBy={20}/>
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={100}/>
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={-1}/>
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={-5}/>
 <ByTen valueByten={value} setValueByten={setValue} increaseBy={-10}/>
-<ByTen valueByten={value} setValueByten={setValue} increaseBy={-20}/>
+<ByTen valueByten={value} setValueByten={setValue} increaseBy={-100}/>
 <h1>Result:{value}</h1>
     </div>
   )
